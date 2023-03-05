29b4bbaa97187d0f91c2cdb9a2b7f5f109558602
FAITH-ORAN <berrichif07>
2023-03-05 23:23:07 +0100

    correction

--- controllers/auth.js
+++ controllers/auth.js
@@ -186,10 +186,7 @@ const sendTokenRes = (user,statusCode,res)=>{
     expires: new Date(Date.now()+process.env.COOKIE_EXPIRE * 24* 60*60*1000),
     httpOnly:true
   }
-  if (process.env.NODE_ENV ==="production"){
-    options.secure = true
-  }
-
+  
   res
     .status(statusCode)
     .cookie("token",token,options)
